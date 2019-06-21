97e6a6ef87bde17214fa366b06174677d1f84c64
Sasha Kayola <ok515@nyu.edu>
2019-06-21 17:51:12 -0400

    added user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,2 +1,30 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+const crypto = require('crypto');
 
+const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    get() {
+      return () => this.getDataValue('password');
+    }
+  }
+});
 
+module.exports = User;
