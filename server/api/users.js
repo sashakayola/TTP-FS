5c8cd54fcb301bc4df1ce6f4a6f428c88a23fc9a
Sasha Kayola <ok515@nyu.edu>
2019-06-21 19:10:11 -0400

    added backend route to add new user

--- server/api/users.js
+++ server/api/users.js
@@ -5,13 +5,27 @@ module.exports = router
 router.get('/:userId', async (req, res, next) => {
   try {
     const userId = req.params.userId;
-    const user = User.findByPk(userId);
-    res.send({
+    const user = await User.findByPk(userId);
+    res.status(200).json({
       id: user.id,
       firstName: user.firstName,
       lastName: user.lastName,
       email: user.email
     })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newUser = User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password
+    })
+    res.status(201).json(newUser)
   } catch (error) {
     next(error)
   }
